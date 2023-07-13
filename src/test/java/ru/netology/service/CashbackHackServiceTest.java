1bff50ed095a5aba148f3f19494a4f30e5d918a4
Olga <o.n.p@mail.ru>
2023-07-13 11:55:46 +0300

    Add test junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,98 +1,100 @@
 package ru.netology.service;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 
 public class CashbackHackServiceTest {
 
     CashbackHackService service = new CashbackHackService();
 
     @Test
-    void minAmount() {
+    public void minAmount() {
 
         int amount = 1;
 
         int expected = 999;
         int actual = service.remain(amount);
 
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
-    void minAmountPlusOne() {
+    public void minAmountPlusOne() {
 
         int amount = 2;
 
         int expected = 998;
         int actual = service.remain(amount);
 
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
-    void AmountLessBoundary() {
+    public void AmountLessBoundary() {
 
         int amount = 600;
 
         int expected = 400;
         int actual = service.remain(amount);
 
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
-    void AmountEqualBoundaryMinusOne() {
+    public void AmountEqualBoundaryMinusOne() {
 
         int amount = 999;
 
         int expected = 1;
         int actual = service.remain(amount);
 
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
-    void AmountEqualBoundary() {
+    public void AmountEqualBoundary() {
 
         int amount = 1000;
 
         int expected = 0;
         int actual = service.remain(amount);
 
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
-    void AmountEqualBoundaryPlusOne() {
+    public void AmountEqualBoundaryPlusOne() {
 
         int amount = 1001;
 
         int expected = 999;
         int actual = service.remain(amount);
 
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
-    void AmountMoreBoundary() {
+    public void AmountMoreBoundary() {
 
         int amount = 3_550;
 
         int expected = 450;
         int actual = service.remain(amount);
 
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
-    void AmountBigMoreBoundary() {
+    public void AmountBigMoreBoundary() {
 
         int amount = 2_556_594;
 
         int expected = 406;
         int actual = service.remain(amount);
 
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
 }
