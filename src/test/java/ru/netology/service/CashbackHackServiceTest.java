ba0f8ae8a960e8721a5674aeee9ece9cc5c2b3cd
Olga <o.n.p@mail.ru>
2023-07-13 15:57:05 +0300

    Add tests Junit Jupiter

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -3,6 +3,7 @@ package ru.netology.service;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 
 public class CashbackHackServiceTest {
@@ -32,7 +33,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void AmountLessBoundary() {
+    public void amountLessBoundary() {
 
         int amount = 600;
 
@@ -43,7 +44,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void AmountEqualBoundaryMinusOne() {
+    public void amountEqualBoundaryMinusOne() {
 
         int amount = 999;
 
@@ -54,7 +55,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void AmountEqualBoundary() {
+    public void amountEqualBoundary() {
 
         int amount = 1000;
 
@@ -65,7 +66,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void AmountEqualBoundaryPlusOne() {
+    public void amountEqualBoundaryPlusOne() {
 
         int amount = 1001;
 
@@ -76,7 +77,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void AmountMoreBoundary() {
+    public void amountMoreBoundary() {
 
         int amount = 3_550;
 
@@ -87,7 +88,7 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void AmountBigMoreBoundary() {
+    public void amountBigMoreBoundary() {
 
         int amount = 2_556_594;
 
@@ -95,6 +96,95 @@ public class CashbackHackServiceTest {
         int actual = service.remain(amount);
 
         Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void minAmountJunitJupiter() {
+
+        int amount = 1;
+
+        int expected = 999;
+        int actual = service.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void minAmountPlusOneJunitJupiter() {
+
+        int amount = 2;
+
+        int expected = 998;
+        int actual = service.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void amountLessBoundaryJunitJupiter() {
+
+        int amount = 600;
+
+        int expected = 400;
+        int actual = service.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void amountEqualBoundaryMinusOneJunitJupiter() {
+
+        int amount = 999;
+
+        int expected = 1;
+        int actual = service.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void amountEqualBoundaryJunitJupiter() {
+
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void amountEqualBoundaryPlusOneJunitJupiter() {
+
+        int amount = 1001;
+
+        int expected = 999;
+        int actual = service.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void amountMoreBoundaryJunitJupiter() {
+
+        int amount = 3_550;
+
+        int expected = 450;
+        int actual = service.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void amountBigMoreBoundaryJunitJupiter() {
+
+        int amount = 2_556_594;
+
+        int expected = 406;
+        int actual = service.remain(amount);
+
+        Assertions.assertEquals(expected, actual);
     }
 
 }
